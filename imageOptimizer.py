e6cbf33270e4bb3cbc34dfd05146675a57c1dd79
Fred C <fredericcharette@gmail.com>
2015-02-03 14:47:39 -0500

    updated imageOptimizer (tinyPNG API)
    manifest builder update in progress -- CURRENTLY BROKEN--

--- imageOptimizer.py
+++ imageOptimizer.py
@@ -1,43 +1,163 @@
+'''
+
+#-------------------------------------------------------------------#
+
+Image Optimizer
+
+This software crawls through the provided media folder and sends
+every png and jpg it finds to the tinyPNG/tinyJPG API.
+
+Copyright 2014
+
+#-------------------------------------------------------------------#
+
+'''
+
+#-----------#
+#  Imports	#
+#-----------#
+
 import os, sys
-from PIL import Image
+from os.path import dirname
+from urllib2 import Request, urlopen
+from base64 import b64encode
+from datetime import datetime
 
-totalsaved = 0
+#-----------#
+# Variables	#
+#-----------#
 
-def optimize_images(dir):
+TOTAL_FILES = 0
+TOTAL_SAVED = 0
 
-    count = 0
+ACCEPTED_FILES = ['.png', '.PNG', '.jpg', '.JPEG', '.JPG', '.jpeg'] #perhaps exessive, we'll see
+
+API_KEY = 'X9ioXjDy63-BRspyK78UFwRrfDO-W1D4' #Frederic's free trial key
+API_URL = 'https://api.tinypng.com/shrink'
+API_CERT = None
+# Uncomment below if you have trouble validating our SSL certificate.
+# Download cacert.pem from: http://curl.haxx.se/ca/cacert.pem
+# cafile = dirname(__file__) + "/cacert.pem"
+
+#-----------#
+#  Classes	#
+#-----------#
+
+#CLI Color codes
+class Colors:
+    PATH = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+#-----------#
+#  Methods	#
+#-----------#
+
+#Walks in the media directory, looking for .png and .jpg files.
+def collect_images(dir):
+
+    global TOTAL_FILES
+
     for root, dirs, files in os.walk(dir):
-        path = root.split("/")
-        print (len(path) - 1) *'---' , os.path.basename(root)       
+        path = root.split('/')      
         for file in files:
-            if ".png" in file:
-                resave_image(root +"/"+ file)
-                count += 1
-            if ".jpg" in file:
-                resave_image(root +"/"+ file)
-                count += 1
-    print " "
-    print "Done!"
-    print "Optimized %d image files" % count
-    
+            for i in range(len(ACCEPTED_FILES)):
+                if (ACCEPTED_FILES[i] in file):
+                    optimize_image(os.path.join(root, file))
+                    TOTAL_FILES += 1
 
-def resave_image(file):
-    global totalsaved
-    print " "
-    print "optimizing " + file + "..."
+
+#Checks filesize changes
+def optimize_image(file):
+
+    global TOTAL_SAVED
+    tstart = datetime.now()
     prevsize = os.path.getsize(file)
-    image = Image.open(file)
-    image.save(file,quality=85,optimize=True)
-    print str(prevsize) + " -----> " + str(os.path.getsize(file))
-    saved = prevsize - os.path.getsize(file)
-    totalsaved += saved
-    if((prevsize - os.path.getsize(file)) < 0):
-        print " Failed! File gained " +str(saved) + " bytes"
+    newsize = 0
+    saved = 0
+    tend = 0
+    delta = 0
+
+    print 'optimizing ' + Colors.PATH + file + Colors.ENDC
+    send_request(file)
+
+    newsize = os.path.getsize(file)
+    tend = datetime.now()
+    delta = tend - tstart
+    print '  process completed in ' + str(int(delta.total_seconds() * 1000)) + 'ms'
+
+    saved = prevsize - newsize
+    TOTAL_SAVED += saved
+    if(saved < 0):
+        print '  ' + str(prevsize) + ' --> ' + str(newsize) + Colors.WARNING + ' (+' + str(get_percent(prevsize, saved)) + '%)' +Colors.ENDC
     else:
-        print " Success! File lost " + str(saved) + " bytes (" + str(round((int(os.path.getsize(file))/int(prevsize))*100, 2)) + "%)"
+        print '  ' + str(prevsize) + ' --> ' + str(newsize) + Colors.GREEN + ' (-' + str(get_percent(prevsize, saved)) + '%)' +Colors.ENDC
+    print ''
 
 
-if __name__ == "__main__":
-    print "Compressing images..."
-    optimize_images(sys.argv[1])
-    print "Saved a total of " + str(totalsaved) + " bytes"
+#Calculates percentage gains or lost
+def get_percent(original, saved):
+
+    ori = int(original) + 0.0
+    opt = int(saved) + 0.0
+    hundred = 100.0
+
+    if(opt < 0):
+        return round(((opt*-1)/ori)*hundred, 2)
+    else:
+        return round((opt/ori)*hundred, 2)
+
+
+#Sends the image optimization request
+def send_request(url):
+
+    request = Request(API_URL, open(url, 'rb').read())
+    auth = b64encode(bytes('api:' + API_KEY)).decode('ascii')
+    request.add_header('Authorization', 'Basic %s' % auth)
+    handle_response(url, urlopen(request))
+
+
+#Handles the optimization request response
+def handle_response(url, response):
+
+    if response.getcode() == 201:
+    	headers = response.info().headers
+    	for i in range(len(headers)):
+    	    if ('Location: ' in headers[i]):
+    	        remote = headers[i][10:].strip()
+                save_to_disk(url, remote)
+    else:
+        print Colors.FAIL +' Error loading '+ url+Colors.ENDC
+    response.close()
+
+
+#Saves the response image to disk, overwriting the original one 
+def save_to_disk(url, remote):
+
+    result = urlopen(remote).read()
+    open(url, "wb").write(result)
+
+
+#Entry point
+def welcome():
+
+    print ''
+    print '### IMAGE OPTIMIZATION ###'
+    print '--------------------------'
+    print ''
+    collect_images(sys.argv[1])
+
+
+#Wrap up
+def goodbye():
+
+	print '### COMPLETED ###'
+	print '-----------------'
+	print ''
+	print 'Total files optimized: '+str(TOTAL_FILES)
+	print 'Total bytes saved: '+str(TOTAL_SAVED) + Colors.ENDC
+
+welcome()
--- manifestBuilder.py
+++ manifestBuilder.py
@@ -1,38 +1,200 @@
-import os, sys
+#!/usr/bin/python
 
-pre = '{"images":['
-post = ']}'
+"""
 
-def scan_dir(dir):
-    create_manifest(dir, "")
-    for root, dirs, files in os.walk(dir):  
-        for folder in dirs:
-            create_manifest(folder, root)
+Simple Manifest Builder
 
+Author:
+    Frederic Charette <fredericcharette@gmail.com>
 
-def create_manifest(dir, r):
-    print "    Creating manifest for " + dir
-    final = pre
-    count = 0
-    for item in os.listdir(os.path.join(r, dir)):
-        if os.path.isfile(os.path.join(r, dir, item)):
-            final += '{"name":"'+item+'", "size":'+str(os.path.getsize(os.path.join(r, dir, item)))+'},'
-            count += 1
-    print ("    %d files added to manifest " + dir) % count
-    final = final[:-1]
-    final += post
+Version:
+    1.0.2
 
-    print "    Writing..."
-    f = open(os.path.join(r, dir , "manifest.json"),'w+')
-    f.write(final)
-    f.close()
-    print "    Manifest completed!"
-    print " "
+Repository:
+    https://github.com/fed135/devtools
 
-if __name__ == "__main__":
+Copyright:
+    GNU Licence 2014-2015
+
+Description:
+    Allows you to scan a folder structure and generate one global 
+    manifest, or multiple ones in each folder. 
+
+Running:
+    python manifestBuilder.py
+
+Options:
+    -a    file paths will be absolute instead of relative
+    -d    includes directories in the manifest
+    -l    will output a local manifest in each folder instead of a global one
+    -s    adds file size in the manifest (bytes)
+    -v    verbose mode
+
+Roadmap:
+    -Add filtering option for specific file types/file names, etc.
+    -Add option to change output file name/location
+    -Add help method
+
+"""
+
+import json
+import os
+import sys
+
+#The list of command line options
+OPTIONS = {
+    '-a':'absolute',
+    '-d':'directories',
+    '-l':'local',
+    '-s':'size',
+    '-v':'verbose'
+}
+
+#The list of colors for cli output
+COLORS = {
+    'BLUE' : '\033[94m',
+    'GREEN' : '\033[92m',
+    'YELLOW' : '\033[93m',
+    'RED' : '\033[91m',
+    'ENDC' : '\033[0m'
+}
+
+class ManifestBuilder(object):
     
-    print "==================================="
-    print "Scanning directory..."
-    scan_dir(sys.argv[1])
-    print "Scan completed!"
+    """
+    Main class of the program.
     
+    Crawls through the folder structure and outputs the manifest file(s)
+
+    Args:
+        settings: a Data structure containing the options passed to
+            the program.
+    """
+
+    global_manifest=[]
+    local_manifest=[]
+
+    def __init__(self, settings={dir:'.'}):
+        self.settings = settings
+        self.start_scan(settings.dir)
+
+
+    def start_scan(self, dir):
+
+        """
+        Initiates the recursive crawl through the directories.
+
+            Args:
+                dir: The directory to start the scan in
+                
+        """
+
+        self.log('Scanning directory...')
+        self.scan_directory(dir, '', self.global_manifest)
+        for root, dirs, files in os.walk(dir):
+            for folder in dirs:
+                self.scan_directory(folder, root, self.global_manifest)
+
+        
+        self.log('Scan completed!')
+
+
+    def scan_directory(self, dir, root, manifest):
+
+        """
+        Scans the provided directory and adds the files to the 
+        appropriate manifest.
+
+            Args:
+                dir: The directory to scan
+                root: The location of the root directory
+                manifest: The manifest to add the files to
+                
+        """
+
+        fullpath = ""
+        isfile = False
+        entry = {}
+
+        for item in os.listdir(os.path.join(root, dir)):
+            fullpath = os.path.join(root, dir, item)
+            isfile = os.path.isfile(fullpath)
+            entry = {}
+
+            
+            #Add entry path to manifest
+            if self.settings.absolute :
+                entry['path'] = fullpath
+            else :
+                entry['path'] = item
+
+            if isfile == False & self.settings.directories == False:
+                continue
+
+            #Add entry size to manifest
+            if self.settings.size & isfile:
+                entry['size'] = os.path.getsize(fullpath)
+
+            manifest.append(entry)
+
+
+    def log(self, msg='', color=COLORS['ENDC']):
+
+        """
+        Outputs a string in the cli, only in verbose mode
+
+            Args:
+                msg: The string to output
+                color: The color to put the string into
+        """
+
+        if self.settings.verbose:
+            print color+msg+COLORS['ENDC']
+
+    def write_manifest(self, dir, root, manifest):
+
+        """
+        Writes the manifest file at the specified location
+
+            Args:
+                dir: The directory of the manifest
+                root: The reference to the root of the scan
+                manifest: The manifest object
+        """
+
+        self.log('Creating manifest for ' + dir, COLORS['GREEN'])
+        f = open(os.path.join(root, dir , 'manifest.json'),'w+')
+        f.write(json.dumps(manifest))
+        f.close()
+        self.log('Manifest completed!')
+
+
+class ManifestBuilderSettings(object):
+
+    """
+    Builds the settings object to be given to the main program
+
+    Args:
+        arguments: The arguments passed to the program
+    """
+
+    dir=""
+
+    def __init__(self, arguments=[]):
+
+        #TODO(fed135): Make this smarter. Need better detection of the
+        #    directory parameter
+        self.dir = arguments[1]
+
+        for x in OPTIONS:
+            setattr(self, OPTIONS[x], (x in arguments))
+
+
+def main():
+
+    settings = ManifestBuilderSettings(sys.argv)
+    ManifestBuilder(settings)
+
+
+if __name__ == '__main__':
+    main()
